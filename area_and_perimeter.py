95351de089dbb641ef1f18b6bfacd062c0118c03
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-03-03 02:30:32 +0000

    lint error

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -12,8 +12,8 @@ def main():
     print("If a rectangle has the dimensions: ")
     print("5cm x 3cm")
     print("")
-    print("Area is {}cm².".format(3*5))
-    print("Perimeter is {}cm.".format(2*(3+5)))
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2*(3 + 5)))
 
 
 if __name__ == "__main__":
