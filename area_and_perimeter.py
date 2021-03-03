9a59debe7e545b4667323313debc822a8a99a8f6
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-03-03 02:35:27 +0000

    lint error

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -13,7 +13,7 @@ def main():
     print("5cm x 3cm")
     print("")
     print("Area is {}cm².".format(3 * 5))
-    print("Perimeter is {}cm.".format(2*(3 + 5)))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
 
 
 if __name__ == "__main__":
